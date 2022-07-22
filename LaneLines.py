a472d4d9e3b034329bea5ecabfbe64acf48fcb9e
hoanghoanghust2006 <hoanghoanghust2006@gmail.com>
2022-07-23 00:02:26 +0700

    6

--- LaneLines.py
+++ LaneLines.py
@@ -220,8 +220,6 @@ class LaneLines:
         out_img[:H, :W] = widget
 
         direction = max(set(self.dir), key = self.dir.count)
-        msg = "Keep Straight Ahead"
-
         if direction == 'L':
             y, x = self.left_curve_img[:,:,3].nonzero()
             out_img[y, x-100+W//2] = self.left_curve_img[y, x, :3]
@@ -235,13 +233,10 @@ class LaneLines:
             out_img[y, x-100+W//2] = self.keep_straight_img[y, x, :3]
 
         cv2.putText(out_img, msg, org=(10, 240), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-       
+        if direction in 'LR':
+            cv2.putText(out_img, curvature_msg, org=(10, 280), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
-     
-
-
-
-
+  
         return out_img
 
     def measure_curvature(self):
